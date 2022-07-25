b1886d489ff6d3d1afa2ee9e449a3b6f0ed70c22
Ivan Villarrubia <ivan@toximosi.es>
2022-07-25 16:06:15 +0200

    fix utils.js

--- 05-Entrega/src/utils.js
+++ 05-Entrega/src/utils.js
@@ -4,10 +4,10 @@
 //---------------------------------------------------
 
 //import
-import {fileURLToPatch} from 'url';
 import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPatch(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default __dirname;
