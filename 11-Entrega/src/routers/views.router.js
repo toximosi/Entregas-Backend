7142a9c932a0d6458ccc093b396bd692bff03450
ivan <toximosi@gmail.com>
2022-09-13 11:45:17 +0200

    prepare normalization

--- 11-Entrega/src/routers/views.router.js
+++ 11-Entrega/src/routers/views.router.js
@@ -4,25 +4,49 @@ import Managers from "../services/manager.service.js";
 const man = new Managers;
 import CreateElement from '../services/createElement.service.js';
 const create = new CreateElement;
+import Normalize from "../services/normalize.service.js";
+const norm = new Normalize;
 import readFile from '../services/readFiles.service.js';
 const file = new readFile;
 import __dirname from '../utils.js';
-const bdJson = __dirname + '/json/products.json';
+const productJson = __dirname + '/json/products.json';
+const messagesJson = __dirname + '/json/messages.json';
 
 //CODE ------------------------------------------------------
 router.get('/api/productos-test', async (req, res) => {
     try {
-        
-        let areData = await file.read(bdJson);
+        let dataIni = await file.read(productJson);
         let Arr = [];
-        if (areData.length == 0) { 
+        if (dataIni.length == 0) { 
             Arr = create.products(5) ;
-            await file.write(bdJson, Arr);
+            await file.write(productJson, Arr);
         } else {
-            Arr = areData;
+            Arr = dataIni;
         } 
         res.render('products', {Arr});
     } catch (err) { console.log(err) };
+});
+
+
+router.get('/api/normalize', async (req, res) => {
+    try {
+        let dataIni = await file.read(messagesJson);
+        let Arr = [];
+        if (dataIni.length == 0) {
+            Arr = create.messages(5);
+            await file.write(messagesJson, Arr);
+        } else { 
+            Arr = dataIni;
+        }
+        const normArr = await norm.menssage(Arr);
+        res.render('normalize', {Arr})
+    } catch (err) { console.log(err);}
+});
+
+
+router.get('/chat', async (req, res) => {
+
+        res.render('chats');
 });
 
 export default router;
--- 11-Entrega/src/services/createElement.service.js
+++ 11-Entrega/src/services/createElement.service.js
@@ -1,16 +1,18 @@
 import faker from 'faker';
-
+import __dirname from '../utils.js';
+import readFile from '../services/readFiles.service.js';
+const file = new readFile;
+const usersJson = __dirname + '/json/users.json';
 
 export default class CreateElement { 
 
     constructor() { }
 
     products = (cant = 20) => { 
-        console.log('create')
         faker.locale = 'es';
 
         /* const { id, timestamp, name, description, code, image, price, stock } = faker; */
-        const { commerce , random , image, } = faker;
+        const { commerce , random , image } = faker;
 
         let data = [];
 
@@ -29,7 +31,32 @@ export default class CreateElement {
         return data;
     }
 
-    users = (cant = 20) => { 
+    messages = (cant = 20) => { 
+        faker.locale = 'es';
+    
+        /* const { id, timestamp, name, description, code, image, price, stock } = faker; */
+        const { name, random, internet,  image , lorem } = faker;
+
+        let data = [];
+
+        for (let i = 0; i < cant; i++) { 
+            data.push({
+                author: {
+                    id: internet.email(), 
+                    nombre: name.firstName(), 
+                    apellido: name.lastName(), 
+                    edad: i+10, 
+                    alias: random.word,
+                    avatar: image.avatar()
+                },
+                text: lorem.paragraph(),
+            })
+        }
+        return data;
+}
+
+
+    userCreatemessages = (cant = 20) => { 
 
         faker.locale = 'es';
 
@@ -40,40 +67,31 @@ export default class CreateElement {
 
         for (let i = 0; i < cant; i++) { 
             data.push({
-                id: internet.email, 
-                nombre: name.firstName, 
-                apellido: name.lastName, 
+                id: internet.email(), 
+                nombre: name.firstName(), 
+                apellido: name.lastName(), 
                 edad: i+10, 
-                alias: lorem.text,
-                avatar: image.avatar
+                alias: lorem.text(),
+                avatar: image.avatar()
             })
         }
         return data;
     }
-
-
-    messages = (cant = 20) => { 
-
-        faker.locale = 'es';
+    users = (obj)=>{ 
+        let users = [];
+        for (let i = 0; i < obj.lenght; i++) { 
+            users.push({
+                id: obj.id, 
+                nombre: obj.nombre, 
+                apellido: obj.apellido, 
+                edad: obj.edad, 
+                alias: obj.alias,
+                avatar: obj.avatar
+            })
+        }
+        file.write(usersJson, users)
+        return users;
+    }
     
-        /* const { id, timestamp, name, description, code, image, price, stock } = faker; */
-        const { name , random, internet,  image , lorem } = faker;
-
-        let data = [];
-
-        for (let i = 0; i < cant; i++) { 
-            data.push({
-                author: {
-                    id: internet.email, 
-                    nombre: name.firstName, 
-                    apellido: name.lastName, 
-                    edad: i+10, 
-                    alias: lorem.text,
-                    avatar: image.avatar
-                },
-                text: random.text,
-            })
-        }
-        return data;
-    }
+    
 }
