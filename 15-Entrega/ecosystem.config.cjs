19e2944e7b09d18d454072c203eb71f12d2a4fd8
ivan <toximosi@gmail.com>
2022-10-12 17:10:18 +0200

    ngnix

--- 15-Entrega/ecosystem.config.cjs
+++ 15-Entrega/ecosystem.config.cjs
@@ -1,27 +1,30 @@
 module.exports = {
   apps: [
       {
-      name   : "server-fork-8080",
-      script: "src/app.js",
-      env: {
-        PORT: 8080
-      }
-    },
-    {
       name   : "server-fork-8081",
       script: "src/app.js",
       env: {
         PORT: 8081
-      }
-    },{
-      name: "server-cluster-8082",
+      },
+      watch:true,
+    },
+    {
+      name   : "server-fork-8082",
       script: "src/app.js",
       env: {
         PORT: 8082
       },
+      watch:true,
+    },{
+      name: "server-cluster-8083",
+      script: "src/app.js",
+      env: {
+        PORT: 8083
+      },
       exec_mode: 'cluster',
       nod_args: "--harmony",
-      instances: 8
+      instances: 8,
+      watch:true,
     }
   ]
 }
--- 15-Entrega/src/app.js
+++ 15-Entrega/src/app.js
@@ -13,27 +13,14 @@ const CPUs = os.cpus().length;//nos trae información de cada uno de los nucleos
 const PORT = process.env.PORT  || 8080;
 const app = express();
 
-//CLUSTER ------------------------------------------------------------------------- INICIO
-if (cluster.isPrimary) {
-    console.log('  -> Soy un proceso primario con pid: ' + process.pid);
-    for (let i = 0; i < CPUs; i++) { cluster.fork() };
-    cluster.on('exit', worker => { 
-        console.log(`Murio proceso worker con pid ${worker.process.pid}`);
-    })
-} else { 
-    console.log('  -> Soy un proceso worker con pid: ' + process.pid);
-    app.listen(PORT, ()=>{console.log(`👽 Now listenig`)});
-}
+const server = app.listen(PORT, ()=>{
+    console.log(`👽 Now listenig on 👉 ${server.address().port}`)
+});
+server.on("error", error => console.log(`Error en el servidor ${error}`)); 
 //CLUSTER ------------------------------------------------------------------------- FIN
-
-//server.on("error", error => console.log(`Error en el servidor ${error}`)); 
-
-
 
 
 //ROUTERS ------------------------------------------------------------------------- INICIO
 app.get('/', (req, res) => { 
     res.send('id process: '+ process.pid + '<br> in port: ' + PORT)
 })
-
-//ROUTERS ------------------------------------------------------------------------- FIN
