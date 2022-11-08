7567be06bf12695c5990f650127618e58f20726b
ivan <toximosi@gmail.com>
2022-11-08 22:24:42 +0100

    creacion de middlewares
    limitacion de acceso

--- 34-Entrega/src/routers/views.routers.js
+++ 34-Entrega/src/routers/views.routers.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import viewsController from '../controllers/views.controller.js';
+import { privateValidation, publicValidation } from '../middlewares/auth.js';
 
 const router = Router();
 
 
 /* REF: app.use('/', viwesRouter); */
-router.get('/',viewsController.home);
-router.get('/register',viewsController.register);
-router.get('/login',viewsController.login);
+router.get('/', privateValidation, viewsController.home);
+router.get('/register', publicValidation, viewsController.register);
+router.get('/login', publicValidation, viewsController.login);
 
 
 export default router;
