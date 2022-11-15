80379bb27f1cc35c6dc4fef72df10d2e11c45936
ivan <toximosi@gmail.com>
2022-11-16 00:13:40 +0100

    actualizar menu y eliminar limitaciones de acceso

--- 34-Entrega_new/src/routers/views.routers.js
+++ 34-Entrega_new/src/routers/views.routers.js
@@ -6,13 +6,13 @@ import uploader from '../services/uploader.js';
 
 
 /* REF: app.use('/', viwesRouter); */
-router.get('/', privateValidation, viewsController.home);
-router.get('/register',uploader.single('image'),  publicValidation, viewsController.register);
-router.get('/login', publicValidation, viewsController.login);
-router.get('/logout', publicValidation, viewsController.logout);
-router.get('/productList', publicValidation, viewsController.productList);
+router.get('/', /* privateValidation,  */viewsController.home);
+router.get('/register',uploader.single('image'),  /* publicValidation,  */viewsController.register);
+router.get('/login', /* publicValidation,  */viewsController.login);
+router.get('/logout',/*  publicValidation, */ viewsController.logout);
+router.get('/productList', /* publicValidation, */ viewsController.productList);
 router.get('/carts', /* publicValidation, */ viewsController.carts);
-router.get('/perfil', publicValidation, viewsController.perfil);
+router.get('/perfil', /* publicValidation, */ viewsController.perfil);
 
 router.get('/productCreate',/* privateValidation, */ /* executePolices(['ADMIN']), */ viewsController.productCreate);
 router.get('/productList',/* privateValidation, */ /* executePolices(['ADMIN']), */ viewsController.productList);
