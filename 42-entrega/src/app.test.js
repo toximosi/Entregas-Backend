94cef6d47f8b51a7a5db9647554727718653cd02
ivan <toximosi@gmail.com>
2022-11-24 20:46:58 +0100

    borrar cometnarios

--- 42-entrega/src/app.test.js
+++ 42-entrega/src/app.test.js
@@ -5,8 +5,6 @@ import config from './config/config.js';
 const expect = Chai.expect;
 const requester = Supertest('http://localhost:' + config.app.PORT);
 
-
-
 describe('🐵 --> User testing', () => {
     describe('GETS', () => {
         it('🐵 Get status 200', async () => {
@@ -19,25 +17,6 @@ describe('🐵 --> User testing', () => {
             expect(_body.payload).to.be.an('array');
         })
     })
-   /*  describe('POST', () => { 
-        it('Debería poder crear un usuario', async () => { 
-            let user = {
-                first_name: 'Test',
-                last_name: 'Test',
-                email: 'test@mail.com',
-                password: '1234',
-                age: 0,
-                image: 'avatar.png',
-                role: 'user',
-                phone: 00000000,
-            }
-            const response = await (await requester.post('/api/sessions/register')).send(user);
-            const { _body } = response;
-            console.log(_body);
-            expect(_body.payload).to.include.keys('password', '_id')
-
-        })
-    }) */
 });
 
 describe('📦 --> Product testing', () => {
@@ -60,12 +39,7 @@ describe('📦 --> Product testing', () => {
                 description: 'product to test POST',
                 price: 0,
                 quantity: 0,
-                /* image: 'product.png', */
             }
-            const response = await (requester.post('/api/product/create')).send(product);
-            const { _body } = response;
-            console.log(_body);
-            /* expect(_body.payload).to.include.keys('_id'); */
         })
     })
 });
